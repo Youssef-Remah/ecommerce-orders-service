d18cc9b24bbe41405c961b65dcbb8d5a7bea1bb3
Youssef-Remah <yremah8@gmail.com>
2026-04-07 14:09:28 +0200

    add IOrdersService interface

---
+++ BusinessLogicLayer/ServiceInterfaces/IOrdersService.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogicLayer.DTOs;
+using DataAccessLayer.Entities;
+using MongoDB.Driver;
+
+namespace BusinessLogicLayer.ServiceInterfaces
+{
+    public interface IOrdersService
+    {
+        Task<List<OrderResponse?>> GetOrders();
+        Task<List<OrderResponse?>> GetOrders(FilterDefinition<Order> filter);
+        Task<OrderResponse?> GetOrderByCondition(FilterDefinition<Order> filter);
+        Task<OrderResponse?> AddOrder(OrderAddRequest orderAddRequest);
+        Task<OrderResponse?> UpdateOrder(OrderUpdateRequest orderUpdateRequest);
+        Task<bool> DeleteOrder(Guid orderId);
+    }
+}
