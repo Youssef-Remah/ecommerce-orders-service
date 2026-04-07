21b9007ebd74c38fdfc69050702a54e69d5c5e76
Youssef-Remah <yremah8@gmail.com>
2026-04-07 11:58:09 +0200

    add IOrdersRepository

---
+++ DataAccessLayer/Repositories/OrdersRepository.cs
@@ -0,0 +1,66 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.RepositoryInterfaces;
+using MongoDB.Driver;
+
+namespace DataAccessLayer.Repositories
+{
+    public class OrdersRepository : IOrdersRepository
+    {
+        private readonly IMongoCollection<Order> _orders;
+        private readonly string collectionName = "orders";
+
+        public OrdersRepository(IMongoDatabase mongoDatabase)
+        {
+            _orders = mongoDatabase.GetCollection<Order>(collectionName);
+        }
+
+        public async Task<Order?> AddOrder(Order order)
+        {
+            order.OrderId = Guid.NewGuid();
+            await _orders.InsertOneAsync(order);
+            
+            return order;
+        }
+
+        public async Task<bool> DeleteOrder(Guid id)
+        {
+            var filter = Builders<Order>.Filter.Eq(temp => temp.OrderId, id);
+
+            var order = (await _orders.FindAsync(filter)).FirstOrDefault();
+
+            if(order == null) return false;
+
+            var result = await _orders.DeleteOneAsync(filter);
+
+            return result.DeletedCount > 0;
+        }
+
+        public async Task<Order?> GetOrderByCondition(FilterDefinition<Order> filter)
+        {
+            return (await _orders.FindAsync(filter)).FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<Order>> GetOrders()
+        {
+            return (await _orders.FindAsync(Builders<Order>.Filter.Empty)).ToList();
+        }
+
+        public async Task<IEnumerable<Order?>> GetOrders(FilterDefinition<Order> filter)
+        {
+            return (await _orders.FindAsync(filter)).ToList();
+        }
+
+        public async Task<Order?> UpdateOrder(Order order)
+        {
+            var filter = Builders<Order>.Filter.Eq(temp => temp.OrderId, order.OrderId);
+
+            var orderToUpdate = (await _orders.FindAsync(filter)).FirstOrDefault();
+
+            if (orderToUpdate == null) return null;
+
+            var res = await _orders.ReplaceOneAsync(filter, order);
+
+            return order;
+        }
+    }
+}
